abc690036d0ad1b50db4314d20dd83d0ae4cc080
michal221 <146548792+michallanger22@users.noreply.github.com>
2024-01-27 17:31:04 +0100

    Boot.cs
    CS is

---
+++ Boot.cs
@@ -0,0 +1,31 @@
+using System;
+
+
+
+public class Tutorial
+
+{
+
+    public static void Main()
+
+    {
+
+        string x = "boot";
+
+        int n = 899;
+
+
+
+        for( int i = 0; i < n; i++)
+
+        {
+
+            Console.WriteLine(x);
+
+        }
+
+    }
+
+
+
+}
